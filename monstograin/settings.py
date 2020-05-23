6007a114b564bd43607ea295f2bb0f9ec18a8dc3
Sai Yuva Teja Gorthy <yuvas2001@gmail.com>
2020-05-23 10:33:35 +0530

    Update settings.py

--- monstograin/settings.py
+++ monstograin/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!mhjnt#2_eslm+)!q!kaa+k=lx=ba&@mh526z^sz9zo34ar&7w'
+SECRET_KEY = '*******'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
